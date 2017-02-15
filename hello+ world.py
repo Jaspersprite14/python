880b31d308b7c499cc7c4b4b709396a863007b58
Jaspersprite14 <ellapeipert@gmail.com>
2017-02-15 16:09:27 -0500

    Add files via upload

---
+++ hello+ world.py
@@ -0,0 +1,36 @@
+#Create a tuple
+betakids = ('John', 'Rose', 'Dave', 'Jade')
+print(betakids)
+
+#Access a tuple value
+print(" betakid: ")
+print( betakids[3])
+
+#Create a tuple with new information
+alphakids = ('Jane', 'Roxy', 'Dirk', 'Jake')
+kids = betakids + alphakids
+print("New Tuple: ")
+print(kids)
+
+ #Delete tuple
+del kids
+
+rose = "rose"
+roxy = "roxy"
+
+if rose == roxy:
+    print("rose not are roxy")
+    
+ 
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ house.py
@@ -0,0 +1,34 @@
+# Our Variables
+ROSES_ROOM = "rose's room"
+HALLWAY = "hallway'
+LIVING_ROOM = "living room"
+
+
+
+
+
+def rosesroom():
+     print("You are in rose's room")
+     
+def hallway():
+    print("You are in the hallway")
+
+def livingroom():
+    print("You are in the living room")
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
